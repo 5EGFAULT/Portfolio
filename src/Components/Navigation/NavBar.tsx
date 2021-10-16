4aa466bc3a09072615b5f1269d1bf30bc415a71e
souiniaayoub <souiniaayoub@gmail.com>
2021-10-16 17:54:18 -0400

    before changing all things

--- src/Components/Navigation/NavBar.tsx
+++ src/Components/Navigation/NavBar.tsx
@@ -1,167 +1,97 @@
 import NavItem from "./NavItem";
 import "./NavBar.css";
 import { useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { svgCurve } from "../../util/NavCurving";
 
-var height = window.innerHeight,
-  x = 0,
-  y = height / 2,
-  curveX = 10,
-  curveY = 0,
-  targetX = 0,
-  xitteration = 0,
-  yitteration = 0;
-var hoverZone = 150;
-// var hoverZone = 200;
-var expandAmount = 25;
-
-function easeOutExpo(
-  currentIteration: any,
-  startValue: any,
-  changeInValue: any,
-  totalIterations: any
-) {
-  return (
-    changeInValue *
-      (-Math.pow(2, (-10 * currentIteration) / totalIterations) + 1) +
-    startValue
-  );
-}
 export default function NavBar() {
   const blobref = useRef<SVGSVGElement>(null);
   const blobpathref = useRef<SVGPathElement>(null);
   const hambergerref = useRef<HTMLDivElement>(null);
-
+  const cord = useRef({ x: 0, y: window.innerHeight / 2 });
+  // let x = 2,
+  //   y = window.innerHeight / 2;
   // const hambergerref = useRef<HTMLButtonElement>(null);
   // const [mousepos, setmousepos] = useState({})
   const [open, setopen] = useState(false);
+  const animatecurve = () => {
+    let values = svgCurve(cord.current.x, cord.current.y, open);
 
-  const svgCurve = () => {
-    if (curveX > x - 1 && curveX < x + 1) {
-      xitteration = 0;
-    } else {
-      if (open) {
-        targetX = 0;
-      } else {
-        xitteration = 0;
-        if (x > hoverZone) {
-          targetX = 0;
-        } else {
-          targetX = -(((60 + expandAmount) / 100) * (x - hoverZone));
-        }
-      }
-      xitteration++;
-    }
-
-    if (curveY > y - 1 && curveY < y + 1) {
-      yitteration = 0;
-    } else {
-      yitteration = 0;
-      yitteration++;
-    }
-
-    curveX = easeOutExpo(xitteration, curveX, targetX - curveX, 100);
-    curveY = easeOutExpo(yitteration, curveY, y - curveY, 100);
-
-    var anchorDistance = 200;
-    var curviness = anchorDistance - 40;
-    if (!open) {
-      console.log("test");
-    } else {
-      if (!open) {
-        console.log(open);
-      }
-    }
-    var newCurve2 =
-      "M60," +
-      height +
-      "H0V0h60v" +
-      (curveY - anchorDistance) +
-      "c0," +
-      curviness +
-      "," +
-      curveX +
-      "," +
-      curviness +
-      "," +
-      curveX +
-      "," +
-      anchorDistance +
-      "S60," +
-      curveY +
-      ",60," +
-      (curveY + anchorDistance * 2) +
-      "V" +
-      height +
-      "z";
     if (blobpathref && blobpathref.current)
-      blobpathref.current.setAttribute("d", newCurve2);
+      blobpathref.current.setAttribute("d", values.curve);
 
     if (blobref && blobref.current)
-      blobref.current.setAttribute("width", String(curveX + 60) + "px");
+      blobref.current.setAttribute("width", values.blobwidth);
     if (hambergerref && hambergerref.current)
-      hambergerref.current.style.transform =
-        "translate(" + (curveX + 10) + "px, " + (curveY - 10) + "px)";
+      hambergerref.current.style.transform = values.hambergtransform;
 
-    window.requestAnimationFrame(svgCurve);
+    // window.requestAnimationFrame(animatecurve);
   };
   useEffect(() => {
     document.addEventListener("mousemove", (e) => {
-      x = e.pageX;
-
-      y = e.pageY;
-      // console.log(x);
+      cord.current.x = e.pageX;
+      cord.current.y = e.pageY;
+      // window.requestAnimationFrame(animatecurve);
+      animatecurve();
     });
+    // animatecurve();
     // return () => {
-    //   cleanup
-    // }
+    //   cleanup;
+    // };
   });
-  useEffect(() => {
-    window.requestAnimationFrame(svgCurve);
-  }, [open]);
+  // useEffect(() => {
+  //   window.requestAnimationFrame(() => {
+  //     let values = svgCurve(cord.current.x, cord.current.y, open);
+
+  //     if (blobpathref && blobpathref.current)
+  //       blobpathref.current.setAttribute("d", values.curve);
+
+  //     if (blobref && blobref.current)
+  //       blobref.current.setAttribute("width", values.blobwidth);
+  //     if (hambergerref && hambergerref.current)
+  //       hambergerref.current.style.transform = values.hambergtransform;
+
+  //     window.requestAnimationFrame(animatecurve);
+  //   });
+  // }, [open]);
   return (
-    <>
-      <motion.div
-        className={open ? "nav-bar expanded" : "nav-bar"}
-        onMouseLeave={() => {
-          setopen(false);
+    <div id="menu" className={open ? "expanded" : ""}>
+      <div
+        className="hamburger"
+        ref={hambergerref}
+        onClick={() => {
+          console.log(open);
+
+          setopen(!open);
         }}
       >
-        <div
-          ref={hambergerref}
-          className={open ? "nav-toggle open" : "nav-toggle"}
-          onClick={() => {
-            setopen(!open);
-          }}
-          onMouseEnter={() => {
-            // setopen(true);
-          }}
-        >
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-        </div>
-        <div className="nav-logo">SA.</div>
-        <div className="nav-list">
-          <NavItem link="/" text="About me" />
-          <NavItem link="/resume" text="Resume" />
-          <NavItem link="/projects" text="Projects" />
-        </div>
-        <div className="nav-contact">f w l</div>
-        <svg
-          ref={blobref}
-          version="1.1"
-          id="blob"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-        >
-          <path
-            ref={blobpathref}
-            id="blob-path"
-            d="M60,500H0V0h60c0,0,20,172,20,250S60,900,60,500z"
-          />
-        </svg>
-      </motion.div>
-    </>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+      <div className="menu-inner">
+        <ul>
+          <li>Menu Item</li>
+          <li>Menu Item</li>
+          <li>Menu Item</li>
+          <li>Menu Item</li>
+          <li>Menu Item</li>
+          <li>Menu Item</li>
+        </ul>
+      </div>
+
+      <svg
+        ref={blobref}
+        version="1.1"
+        id="blob"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <path
+          ref={blobpathref}
+          id="blob-path"
+          d="M60,983H0V0h60v-76.5837121932024c0,160,0.29969056167817326,160,0.29969056167817326,200S60,123.4162878067976,60,523.4162878067976V983z"
+        />
+      </svg>
+    </div>
   );
 }
--- src/util/NavCurving.ts
+++ src/util/NavCurving.ts
@@ -1,42 +1,24 @@
-function easeOutExpo(
-    currentIteration: any,
-    startValue: any,
-    changeInValue: any,
-    totalIterations: any
-) {
-    return (
-        changeInValue *
-        (-Math.pow(2, (-10 * currentIteration) / totalIterations) + 1) +
-        startValue
-    );
+
+let height = window.innerHeight,
+    x = 0, y = height / 2,
+    curveX = 10,
+    curveY = 0,
+    targetX = 0,
+    xitteration = 0,
+    yitteration = 0;
+
+function easeOutExpo(currentIteration: number, startValue: number, changeInValue: number, totalIterations: number) {
+    return changeInValue * (-Math.pow(2, -10 * currentIteration / totalIterations) + 1) + startValue;
 }
 
-// let x = 0, y = window.innerHeight / 2;
-// export const docmousemove = () => {
-//     document.addEventListener("mousemove", (e) => {
-//         x = e.pageX;
+let hoverZone = 150;
+let expandAmount = 20;
 
-//         y = e.pageY;
-//         // console.log(x, y);
-
-//     });
-// }
-export const svgCurve = (x: number, y: number,
-    open: boolean,
-) => {
-
-    let curveX = 10;
-    let curveY = 0;
-    let targetX = 0;
-    let xitteration = 0;
-    let yitteration = 0;
-    let hoverZone = 150;
-    let expandAmount = 20;
-    let height = window.innerHeight;
-    if (curveX > x - 1 && curveX < x + 1) {
+export function svgCurve(x: number, y: number, menuExpanded: boolean) {
+    if ((curveX > x - 1) && (curveX < x + 1)) {
         xitteration = 0;
     } else {
-        if (open) {
+        if (menuExpanded) {
             targetX = 0;
         } else {
             xitteration = 0;
@@ -49,7 +31,7 @@ export const svgCurve = (x: number, y: number,
         xitteration++;
     }
 
-    if (curveY > y - 1 && curveY < y + 1) {
+    if ((curveY > y - 1) && (curveY < y + 1)) {
         yitteration = 0;
     } else {
         yitteration = 0;
@@ -62,48 +44,13 @@ export const svgCurve = (x: number, y: number,
     var anchorDistance = 200;
     var curviness = anchorDistance - 40;
 
-    var newCurve2 =
-        "M60," +
-        height +
-        "H0V0h60v" +
-        (curveY - anchorDistance) +
-        "c0," +
-        curviness +
-        "," +
-        curveX +
-        "," +
-        curviness +
-        "," +
-        curveX +
-        "," +
-        anchorDistance +
-        "S60," +
-        curveY +
-        ",60," +
-        (curveY + anchorDistance * 2) +
-        "V" +
-        height +
-        "z";
-    return { newCurve2, widthblob: String(curveX + 60) + "px", transformhamberger: "translate(" + curveX + "px, " + curveY + "px)" };
-    // if (blobpathref && blobpathref.current) {
-    //     blobpathref.current.setAttribute("d", newCurve2);
-    //     // setpathDstate(newCurve2);
-    //     // console.log(blobpathref.current.getAttribute("d"));
-    // }
-    // if (blobref && blobref.current)
-    //     blobref.current.setAttribute("width", String(curveX + 60) + "px");
-    // if (hambergerref && hambergerref.current)
-    //     hambergerref.current.style.transform =
-    //         "translate(" + curveX + "px, " + curveY + "px)";
+    var newCurve2 = "M60," + height + "H0V0h60v" + (curveY - anchorDistance) + "c0," + curviness + "," + curveX + "," + curviness + "," + curveX + "," + anchorDistance + "S60," + (curveY) + ",60," + (curveY + (anchorDistance * 2)) + "V" + height + "z";
 
-    // window.requestAnimationFrame(() => {
+    // blobPath.attr('d', newCurve2);
 
-    //     svgCurve(
-    //         open,
-    //         blobpathref,
-    //         blobref,
-    //         hambergerref,
-    //         setpathDstate
-    //     )
-    // });
-};
+    // blob.width(curveX + 60);
+
+    // hamburger.css('transform', 'translate(' + curveX + 'px, ' + curveY + 'px)');
+
+    return { curve: newCurve2, blobwidth: String(curveX + 60) + 'px', hambergtransform: "translate(" + (curveX + 10) + "px, " + (curveY) + "px)" };
+}
